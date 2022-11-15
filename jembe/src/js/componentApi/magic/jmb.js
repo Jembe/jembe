5aba204c82b6052b3eb71003713882d5932a1fee
Predrag Peranović <predrag.peranovic@blokkod.me>
2022-11-15 22:00:05 +0100

    fix upload array of files

--- jembe/src/js/componentApi/magic/jmb.js
+++ jembe/src/js/componentApi/magic/jmb.js
@@ -1,118 +1,110 @@
 import { isAbsolute, join } from "path";
 export default class JMB {
   constructor(jembeClient, execName) {
-    this.jembeClient = jembeClient
-    this.execName = execName
-    this.callsCommands = false
+    this.jembeClient = jembeClient;
+    this.execName = execName;
+    this.callsCommands = false;
   }
   call(actionName, ...params) {
-    this.callsCommands = true
+    this.callsCommands = true;
 
-    let kwargs = {}
-    let args = []
+    let kwargs = {};
+    let args = [];
     if (params.length === 1 && params[0].constructor == Object) {
-      kwargs = params[0]
+      kwargs = params[0];
     } else {
-      args = params
+      args = params;
     }
-    this.jembeClient.addCallCommand(
-      this.execName,
-      actionName,
-      args,
-      kwargs
-    )
+    this.jembeClient.addCallCommand(this.execName, actionName, args, kwargs);
   }
   display() {
-    this.call("display")
+    this.call("display");
   }
   /**
    *  Change state param of current component,
-   *  by addig initialise command with only state param 
+   *  by addig initialise command with only state param
    *  defined by stateName changed to newlly provided value
    * stateName can use dots (.) to set attribure of object
-   * Examples: "someObject.paramName" 
-   * @param {string} stateName 
-   * @param {*} value 
+   * Examples: "someObject.paramName"
+   * @param {string} stateName
+   * @param {*} value
    */
   set(stateName, value) {
-    this.callsCommands = true
-    if (value instanceof FileList || value instanceof File) {
-      this.jembeClient.addFilesForUpload(this.execName, stateName, value)
+    this.callsCommands = true;
+    if (
+      value instanceof FileList ||
+      value instanceof File ||
+      (Array.isArray(value) && value.length > 0 && value[0] instanceof File)
+    ) {
+      this.jembeClient.addFilesForUpload(this.execName, stateName, value);
     } else {
-      let params = {}
-      params[stateName] = value
-      this.jembeClient.addInitialiseCommand(
-        this.execName,
-        params
-      )
+      let params = {};
+      params[stateName] = value;
+      this.jembeClient.addInitialiseCommand(this.execName, params);
     }
   }
   emit(eventName, kwargs = {}, to = null) {
-    this.callsCommands = true
-    this.jembeClient.addEmitCommand(
-      this.execName,
-      eventName,
-      kwargs,
-      to
-    )
+    this.callsCommands = true;
+    this.jembeClient.addEmitCommand(this.execName, eventName, kwargs, to);
   }
   component(relativeExecName, kwargs = {}, mergeExistingParams = true) {
-    this.callsCommands = true
+    this.callsCommands = true;
 
-    let execName = relativeExecName
+    let execName = relativeExecName;
     if (!isAbsolute(relativeExecName)) {
-      execName = join(this.execName, relativeExecName)
+      execName = join(this.execName, relativeExecName);
     }
-    let componentNames = []
-    let startWith = []
-    let index = 0
-    let equalSoFar = true
-    let execNameSplit = execName.split("/")
-    let thisExecNameSplit = this.execName.split("/")
+    let componentNames = [];
+    let startWith = [];
+    let index = 0;
+    let equalSoFar = true;
+    let execNameSplit = execName.split("/");
+    let thisExecNameSplit = this.execName.split("/");
     while (index < execNameSplit.length) {
-      if (equalSoFar === true &&
-        (
-          // if execName is different (including key) we need to genereate init command
-          execNameSplit[index] !== thisExecNameSplit[index] ||
+      if (
+        equalSoFar === true &&
+        // if execName is different (including key) we need to genereate init command
+        (execNameSplit[index] !== thisExecNameSplit[index] ||
           // alsy if kwargs are specified for last component we need do generate init command
-          (index === execNameSplit.length - 1 && kwargs !== {})
-        )
+          (index === execNameSplit.length - 1 && kwargs !== {}))
       ) {
-        equalSoFar = false
+        equalSoFar = false;
       }
       if (!equalSoFar) {
-        componentNames.push(execNameSplit[index])
+        componentNames.push(execNameSplit[index]);
       } else {
-        startWith.push(execNameSplit[index])
+        startWith.push(execNameSplit[index]);
       }
-      index++
+      index++;
     }
 
-    index = 0
+    index = 0;
     while (index < componentNames.length) {
       this.jembeClient.addInitialiseCommand(
         [
           startWith.join("/"),
-          componentNames.slice(0, index + 1).join("/")
+          componentNames.slice(0, index + 1).join("/"),
         ].join("/"),
         index == componentNames.length - 1 ? kwargs : {},
         mergeExistingParams
-      )
-      index++
+      );
+      index++;
     }
-    return new JMB(this.jembeClient, execName)
+    return new JMB(this.jembeClient, execName);
   }
   component_reset(relativeExecName, kwargs = {}) {
-    return this.component(relativeExecName, kwargs, false)
+    return this.component(relativeExecName, kwargs, false);
   }
-  executeCommands(disableInputs=true) {
-    this.jembeClient.executeCommands(disableInputs)
+  executeCommands(disableInputs = true) {
+    this.jembeClient.executeCommands(disableInputs);
   }
   componentsOnPage(filter) {
     if (filter === undefined) {
-      return Object.keys(this.jembeClient.components)
+      return Object.keys(this.jembeClient.components);
     } else {
-      return Object.values(this.jembeClient.components).filter(filter).map(c => c.execName)
+      return Object.values(this.jembeClient.components)
+        .filter(filter)
+        .map((c) => c.execName);
     }
   }
 }
